196c615cdb9346e8163c2c930ad3e6650917b440
robinbenitezmora <robinbenitez1990@gmail.com>
2023-05-15 14:29:03 -0500

    Update controllers and views in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :beers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'beers#index'
 end
